f116fd942ebd0606e84465cfd4d9121696898c30
Ramen Guy <73126759+RamenGuy@users.noreply.github.com>
2025-12-13 13:05:00 -0600

    1.0.3 will be real one day

--- Assets/RiskOfRamen/RiskOfRamenContentProvider.cs
+++ Assets/RiskOfRamen/RiskOfRamenContentProvider.cs
@@ -60,15 +60,17 @@ namespace RiskOfRamen
             _cscWaxWisp = _assetBundle.LoadAsset<CharacterSpawnCard>("cscWaxWisp");
 
             var expansionDef = _assetBundle.LoadAsset<ExpansionDef>("RiskOfRamenExpansion");
+
             RiskOfRamenContentPack.itemDefs.Add(new ItemDef[] { _WaxIdol });
             RiskOfRamenContentPack.itemDefs.Add(new ItemDef[] { _ObsidianCard }); 
             RiskOfRamenContentPack.itemDefs.Add(new ItemDef[] { _DenkuRope });
             RiskOfRamenContentPack.itemDefs.Add(new ItemDef[] { _StainedBelt });
+
             RiskOfRamenContentPack.bodyPrefabs.Add(new GameObject[] { _WaxWispBody });
             RiskOfRamenContentPack.masterPrefabs.Add(new GameObject[] { _WaxWispMaster });
+
             RiskOfRamenContentPack.expansionDefs.Add(new ExpansionDef[] { expansionDef });
 
-            //ShaderUtil.SwapStubbedShadersAsync(_assetBundle);
             SwapAllShaders();
                  
             #region ITEM DISPLAY RULES
@@ -287,217 +289,6 @@ namespace RiskOfRamen
 
 
             //ItemAPI.Add(new CustomItem(_DenkuRope, DenkuRopeDisplay));
-            #endregion
-            #region OBSIDIAN CARD
-            var ObsidianCardDisplay = new ItemDisplayRuleDict();
-
-            ObsidianCardDisplay.Add("mdlCommandoDualies", new ItemDisplayRule[]
-            {
-                    new ItemDisplayRule {
-                        ruleType = ItemDisplayRuleType.ParentedPrefab,
-                        followerPrefab = _ObsidianCard.pickupModelPrefab,
-                        followerPrefabAddress = new UnityEngine.AddressableAssets.AssetReferenceGameObject(""),
-                        limbMask = LimbFlags.None,
-                        childName = "CalfR",
-                        localPos = new Vector3(-0.08452F, 0.2241F, 0.02499F),
-                        localAngles = new Vector3(338.3474F, 98.51011F, 0.91329F),
-                        localScale = new Vector3(0.3F, 0.3F, 0.3F)
-
-                    },
-
-            });
-
-            ObsidianCardDisplay.Add("mdlHuntress", new ItemDisplayRule[]
-            {
-                    new ItemDisplayRule {
-                        ruleType = ItemDisplayRuleType.ParentedPrefab,
-                        followerPrefab = _ObsidianCard.pickupModelPrefab,
-                        followerPrefabAddress = new UnityEngine.AddressableAssets.AssetReferenceGameObject(""),
-                        limbMask = LimbFlags.None,
-                        childName = "UpperArmL",
-                        localPos = new Vector3(-0.08723F, 0.09964F, -0.01873F),
-                        localAngles = new Vector3(27.84832F, 281.4216F, 174.5701F),
-                        localScale = new Vector3(0.3F, 0.3F, 0.3F)
-
-                    },
-
-            });
-
-            ObsidianCardDisplay.Add("mdlBandit2", new ItemDisplayRule[]
-            {
-                    new ItemDisplayRule {
-                        ruleType = ItemDisplayRuleType.ParentedPrefab,
-                        followerPrefab = _ObsidianCard.pickupModelPrefab,
-                        followerPrefabAddress = new UnityEngine.AddressableAssets.AssetReferenceGameObject(""),
-                        limbMask = LimbFlags.None,
-                        childName = "Stomach",
-                        localPos = new Vector3(-0.05635F, 0.04164F, 0.16436F),
-                        localAngles = new Vector3(28.10276F, 156.4939F, 180.7979F),
-                        localScale = new Vector3(0.3F, 0.3F, 0.3F)
-
-                    },
-
-            });
-
-            ObsidianCardDisplay.Add("mdlToolbot", new ItemDisplayRule[]
-            {
-                    new ItemDisplayRule {
-                        ruleType = ItemDisplayRuleType.ParentedPrefab,
-                        followerPrefab = _ObsidianCard.pickupModelPrefab,
-                        followerPrefabAddress = new UnityEngine.AddressableAssets.AssetReferenceGameObject(""),
-                        limbMask = LimbFlags.None,
-                        childName = "UpperArmL",
-                        localPos = new Vector3(-0.02233F, 1.97931F, -0.99985F),
-                        localAngles = new Vector3(2.15264F, 5.1404F, 176.7258F),
-                        localScale = new Vector3(3.5F, 3.5F, 3.5F)
-
-                    },
-
-            });
-
-            ObsidianCardDisplay.Add("mdlEngi", new ItemDisplayRule[]
-            {
-                    new ItemDisplayRule {
-                        ruleType = ItemDisplayRuleType.ParentedPrefab,
-                        followerPrefab = _ObsidianCard.pickupModelPrefab,
-                        followerPrefabAddress = new UnityEngine.AddressableAssets.AssetReferenceGameObject(""),
-                        limbMask = LimbFlags.None,
-                        childName = "LowerArmL",
-                        localPos = new Vector3(0.01394F, 0.13063F, -0.09528F),
-                        localAngles = new Vector3(21.59121F, 341.9269F, 182.462F),
-                        localScale = new Vector3(0.3F, 0.3F, 0.3F)
-
-                    },
-
-            });
-
-            ObsidianCardDisplay.Add("mdlMerc", new ItemDisplayRule[]
-            {
-                    new ItemDisplayRule {
-                        ruleType = ItemDisplayRuleType.ParentedPrefab,
-                        followerPrefab = _ObsidianCard.pickupModelPrefab,
-                        followerPrefabAddress = new UnityEngine.AddressableAssets.AssetReferenceGameObject(""),
-                        limbMask = LimbFlags.None,
-                        childName = "ThighR",
-                        localPos = new Vector3(-0.13783F, 0.10082F, 0.02668F),
-                        localAngles = new Vector3(20.26766F, 267.0182F, 183.088F),
-                        localScale = new Vector3(0.3F, 0.3F, 0.3F)
-
-                    },
-
-            });
-
-            ObsidianCardDisplay.Add("mdlMage", new ItemDisplayRule[]
-            {
-                    new ItemDisplayRule {
-                        ruleType = ItemDisplayRuleType.ParentedPrefab,
-                        followerPrefab = _ObsidianCard.pickupModelPrefab,
-                        followerPrefabAddress = new UnityEngine.AddressableAssets.AssetReferenceGameObject(""),
-                        limbMask = LimbFlags.None,
-                        childName = "UpperArmL",
-                        localPos = new Vector3(0.05757F, 0.15938F, -0.00618F),
-                        localAngles = new Vector3(27.84833F, 281.4216F, 174.5701F),
-                        localScale = new Vector3(0.3F, 0.3F, 0.3F)
-
-                    },
-
-            });
-
-            ObsidianCardDisplay.Add("mdlTreebot", new ItemDisplayRule[]
-            {
-                    new ItemDisplayRule {
-                        ruleType = ItemDisplayRuleType.ParentedPrefab,
-                        followerPrefab = _ObsidianCard.pickupModelPrefab,
-                        followerPrefabAddress = new UnityEngine.AddressableAssets.AssetReferenceGameObject(""),
-                        limbMask = LimbFlags.None,
-                        childName = "FlowerBase",
-                        localPos = new Vector3(-0.26653F, 0.74262F, 0.66245F),
-                        localAngles = new Vector3(25.29461F, 341.2172F, 176.4987F),
-                        localScale = new Vector3(1F, 1F, 1F)
-                    },
-
-            });
-
-            ObsidianCardDisplay.Add("mdlLoader", new ItemDisplayRule[]
-            {
-                    new ItemDisplayRule {
-                        ruleType = ItemDisplayRuleType.ParentedPrefab,
-                        followerPrefab = _ObsidianCard.pickupModelPrefab,
-                        followerPrefabAddress = new UnityEngine.AddressableAssets.AssetReferenceGameObject(""),
-                        limbMask = LimbFlags.None,
-                        childName = "MechHandL",
-                        localPos = new Vector3(-0.09678F, 0.0888F, 0.06277F),
-                        localAngles = new Vector3(28.01476F, 302.2359F, 174.3781F),
-                        localScale = new Vector3(0.3F, 0.3F, 0.3F)
-                    },
-
-            });
-
-            ObsidianCardDisplay.Add("mdlCroco", new ItemDisplayRule[]
-            {
-                    new ItemDisplayRule {
-                        ruleType = ItemDisplayRuleType.ParentedPrefab,
-                        followerPrefab = _ObsidianCard.pickupModelPrefab,
-                        followerPrefabAddress = new UnityEngine.AddressableAssets.AssetReferenceGameObject(""),
-                        limbMask = LimbFlags.None,
-                        childName = "MuzzleHandL",
-                        localPos = new Vector3(-0.79111F, 0.41593F, -1.39771F),
-                        localAngles = new Vector3(45.29366F, 346.007F, 95.27855F),
-                        localScale = new Vector3(2F, 2F, 2F)
-
-                    },
-            });
-
-            ObsidianCardDisplay.Add("mdlCaptain", new ItemDisplayRule[]
-            {
-                    new ItemDisplayRule {
-                        ruleType = ItemDisplayRuleType.ParentedPrefab,
-                        followerPrefab = _ObsidianCard.pickupModelPrefab,
-                        followerPrefabAddress = new UnityEngine.AddressableAssets.AssetReferenceGameObject(""),
-                        limbMask = LimbFlags.None,
-                        childName = "CalfR",
-                        localPos = new Vector3(0.00546F, -0.00302F, -0.06705F),
-                        localAngles = new Vector3(12.07266F, 348.8349F, 189.9568F),
-                        localScale = new Vector3(0.2F, 0.2F, 0.2F)
-
-                    },
-
-            });
-
-            ObsidianCardDisplay.Add("mdlRailGunner", new ItemDisplayRule[]
-            {
-                    new ItemDisplayRule {
-                        ruleType = ItemDisplayRuleType.ParentedPrefab,
-                        followerPrefab = _ObsidianCard.pickupModelPrefab,
-                        followerPrefabAddress = new UnityEngine.AddressableAssets.AssetReferenceGameObject(""),
-                        limbMask = LimbFlags.None,
-                        childName = "TopRail",
-                        localPos = new Vector3(0.00546F, 0.71316F, 0.04062F),
-                        localAngles = new Vector3(345.0563F, 6.60493F, 355.4799F),
-                        localScale = new Vector3(0.2F, 0.2F, 0.2F)
-
-
-                    },
-
-            });
-
-            ObsidianCardDisplay.Add("mdlVoidSurvivor", new ItemDisplayRule[]
-            {
-                    new ItemDisplayRule {
-                        ruleType = ItemDisplayRuleType.ParentedPrefab,
-                        followerPrefab = _ObsidianCard.pickupModelPrefab,
-                        followerPrefabAddress = new UnityEngine.AddressableAssets.AssetReferenceGameObject(""),
-                        limbMask = LimbFlags.None,
-                        childName = "Chest",
-                        localPos = new Vector3(-0.14121F, 0.06264F, 0.18703F),
-                        localAngles = new Vector3(21.73587F, 330.568F, 202.2781F),
-                        localScale = new Vector3(0.2F, 0.2F, 0.2F)
-                    },
-
-            });
-
-
-            ItemAPI.Add(new CustomItem(_ObsidianCard, ObsidianCardDisplay));
             #endregion
 
             #endregion
